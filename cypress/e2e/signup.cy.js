d35737bd3a9c60a3a2f0d284a8991488120ed62e
Andryele Medeiros <andryelemedeiros24@gmail.com>
2024-08-29 13:51:10 -0300

    Configure cypress cloud 16

--- cypress/e2e/signup.cy.js
+++ cypress/e2e/signup.cy.js
@@ -3,10 +3,23 @@ import { faker } from '@faker-js/faker/locale/en'
 describe('Sign up', () => {
   const emailAddress = `${faker.datatype.uuid()}@${Cypress.env('MAILOSAUR_SERVER_ID')}.mailosaur.net`
   const password = Cypress.env('USER_PASSWORD')
-
+  beforeEach(() => {
+    cy.intercept('GET', '**/notes').as('getNotes')
+    cy.visit('/signup')
+    cy.get('#email').type(Cypress.env('USER_EMAIL')) // Use environment variable or some valid test email
+    cy.get('#password').type(Cypress.env('USER_PASSWORD'), { log: false })
+    cy.get('#confirmPassword').type(Cypress.env('USER_PASSWORD'), { log: false })
+    cy.get('button[type="submit"].LoaderButton', { timeout: 10000 }).should('be.disabled').and('be.visible').click()
+    cy.get('#confirmationCode').should('be.visible')
+    cy.mailosaurGetMessage(Cypress.env('MAILOSAUR_SERVER_ID'), {
+      sentTo: Cypress.env('USER_EMAIL')
+    }).then(message => {
+      const confirmationCode = message.html.body.match(/\d{6}/)[0]
+      cy.get('#confirmationCode').type(${confirmationCode}{enter})
+      cy.wait('@getNotes')
+    })
+  })
   it('successfully signs up using confirmation code sent via email', () => {
-    cy.fillSignupFormAndSubmit(emailAddress, password)
-    //cy.wait()
     cy.contains('h1', 'Your Notes').should('be.visible')
     cy.contains('a', 'Create a new note').should('be.visible')
   })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,18 +1,18 @@
 Cypress.Commands.add('fillSignupFormAndSubmit', (email, password) => {
   cy.intercept('GET', '**/notes').as('getNotes')
-  //cy.visit('/signup')
+  cy.visit('/signup')
   cy.get('#email').type(email)
   cy.get('#password').type(password, { log: false })
   cy.get('#confirmPassword').type(password, { log: false })
   cy.get('button[type="submit"].LoaderButton', { timeout: 10000 }).should('be.disabled').and('be.visible')
   cy.get('#confirmationCode').should('be.visible')
-  //cy.mailosaurGetMessage(Cypress.env('MAILOSAUR_SERVER_ID'), {
-  //  sentTo: email
-  //}).then(message => {
-  //  const confirmationCode = message.html.body.match(/\d{6}/)[0]
-  // cy.get('#confirmationCode').type(`${confirmationCode}{enter}`)
-  // cy.wait('@getNotes')
-  //})
+  cy.mailosaurGetMessage(Cypress.env('MAILOSAUR_SERVER_ID'), {
+    sentTo: email
+  }).then(message => {
+    const confirmationCode = message.html.body.match(/\d{6}/)[0]
+    cy.get('#confirmationCode').type(`${confirmationCode}{enter}`)
+    cy.wait('@getNotes')
+  })
 })
 
 Cypress.Commands.add('login', (
