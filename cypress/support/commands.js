9205da5771003267c4ec65ade45057b9c2e46faa
Andryele Medeiros <andryelemedeiros24@gmail.com>
2024-08-29 13:05:40 -0300

    Configure cypress cloud 11

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -4,8 +4,7 @@ Cypress.Commands.add('fillSignupFormAndSubmit', (email, password) => {
   cy.get('#email').type(email)
   cy.get('#password').type(password, { log: false })
   cy.get('#confirmPassword').type(password, { log: false })
-  cy.contains('button', 'Signup').click()
-  cy.get('button[type="submit"].LoaderButton').should('be.visible')
+  cy.get('button[type="submit"].LoaderButton').contains('Signup').should('be.visible').and('contain.text', 'Signup')
   cy.get('#confirmationCode').should('be.visible')
   cy.mailosaurGetMessage(Cypress.env('MAILOSAUR_SERVER_ID'), {
     sentTo: email
