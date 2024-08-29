025cd816830f250626b32241f68aa9516c517e7f
Andryele Medeiros <andryelemedeiros24@gmail.com>
2024-08-29 13:27:22 -0300

    Configure cypress cloud 15

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,6 @@
 Cypress.Commands.add('fillSignupFormAndSubmit', (email, password) => {
   cy.intercept('GET', '**/notes').as('getNotes')
-  cy.visit('/signup')
+  //cy.visit('/signup')
   cy.get('#email').type(email)
   cy.get('#password').type(password, { log: false })
   cy.get('#confirmPassword').type(password, { log: false })
